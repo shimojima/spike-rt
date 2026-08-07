684c993bfca9d4eb37d6f705dcd46ce547e1442c
Makoto Shimojima <shimojima_makoto@nias.ac.jp>
2026-08-07 15:50:35 +0900

    Limit cb_error alert to loop only 10 times then power off

--- drivers/spike/cb_error.h
+++ drivers/spike/cb_error.h
@@ -24,14 +24,15 @@
 #include <kernel.h>
 #include <spike/hub/display.h>
 #include <spike/hub/speaker.h>
+#include <spike/hub/system.h>
 
 // Stop if error is detected, unless port=='X', in which case retval is returned as before.
 #define check_pbio_error_r(port, err, retval) \
   do { \
     if ((err) == PBIO_SUCCESS) break; \
     if ((port) == 'X') return retval; \
-    hub_speaker_set_volume(100); \
-    while (1) { \
+    for (int i = 0; i < 10; i++) { \
+      hub_speaker_set_volume(100-10*i); \
       hub_display_char(port); \
       hub_speaker_play_tone(960.0, 500); \
       hub_display_off(); \
@@ -41,7 +42,10 @@
       hub_display_off(); \
       dly_tsk(150*1000); \
     } \
-  } while (9)
+    hub_speaker_set_volume(0); \
+    hub_display_off(); \
+    hub_system_shutdown(); \
+  } while (0)
 
 #endif
 
